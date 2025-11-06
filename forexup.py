bee210530d28a4601fd5cdb5756890652ab69556
亞文 <60696626+tumalanga@users.noreply.github.com>
2025-11-06 22:46:38 +0800

    Update forexup.py

--- forexup.py
+++ forexup.py
@@ -11,5 +11,5 @@ gsheetkey_before = "1qsyZS3H9S3lcHYyn3TXR7VoCuPLsFHlUL7GRvSNDeGI"
 url_before=f'https://docs.google.com/spreadsheet/ccc?key={gsheetkey}&output=xlsx'
 df_before = pd.read_excel(url,sheet_name="Result")
 df_before['options'] = df_before['currency_name']+" ("+df_before['currency']+")"
-
-pd.concat([df_before,df]).to_pickle("streamlit/assets/rates.pkl")
+keep = pd.concat([df_before,df])
+keep.to_pickle("streamlit/assets/rates.pkl")
