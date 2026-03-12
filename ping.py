2cf46f2d5537a8941df41823a7e858a2f7f2a326
亞文 <60696626+tumalanga@users.noreply.github.com>
2026-03-12 09:56:33 +0800

    ganti webdriver:   ping.py

--- ping.py
+++ ping.py
@@ -19,13 +19,19 @@ def ping():
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--disable-gpu")
+    options.add_argument("--remote-debugging-port=9222")
+    # options.add_argument("--window-size=1920,1080")
     options.binary_location = "/usr/bin/chromium-browser"
 
     print("🚀 Membuka browser...")
     driver = webdriver.Chrome(
-        service=Service(ChromeDriverManager().install()),
+        service=Service("/usr/bin/chromedriver"),
         options=options
     )
+    # driver = webdriver.Chrome(
+    #     service=Service(ChromeDriverManager().install()),
+    #     options=options
+    # )
 
     try:
         # Step 1: Akses URL target
