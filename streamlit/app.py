c4f596b8a101e69adb0da05f3775963f7bcee29e
亞文 <60696626+tumalanga@users.noreply.github.com>
2026-05-17 09:40:18 +0800

    modified:   streamlit/app.py 	modified:   streamlit/pages/lr.py

--- streamlit/app.py
+++ streamlit/app.py
@@ -69,13 +69,8 @@ project_s = st.Page(
     title="Mod CV", visibility="hidden"
 )
 
-project_y = st.Page(
-    page="pages/pls_sem.py",
-    title="Mod CV", visibility="hidden"
-)
-
 pg = st.navigation({"info":[about_page],
                     "Projects": [project_1, project_2, project_3],
-                    "others": [project_x, project_s, project_y]
+                    "others": [project_x, project_s]
                     })
 pg.run()
